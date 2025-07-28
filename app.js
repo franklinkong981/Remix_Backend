8b11ac7daa345e7b29bed0fd1113083ef979f339
Franklin Kong <franklinkong981@gmail.com>
2025-07-28 12:32:26 -0700

    Made folders and wrote app.js skeleton code

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
-const bcrypt = require("bcrypt");
+/* This is the main file of Remix that runs the website.
+*/
 
-async function getHashedPassword() {
-  return await bcrypt.hash("admin2_password", 12);
-}
+const express = require("express");
+const morgan = require("morgan");
 
-getHashedPassword().then(result => {
-  console.log(result);
-});
+//errors imports here
+
+//middleware imports here
+
+//routes imports here
+
+const app = express();
+
+//allow form-encoded and json-encoded parsing.
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//use the morgan middleware logging library, this prints information about each request sent to the server on the terminal/console.
+app.use(morgan('dev'));
+
+//prevents printing favicon.ico error to terminal/console.
+app.get('/favicon.ico', (req, res) => res.sendStatus(204));
+
+//app.use route files here
+
+//404 error handler here
+
+//generic error handler here
+
+module.exports = app;
