f30d4caa52785c100e2b7991c456530c8c42a1e4
Franklin Kong <franklinkong981@gmail.com>
2025-07-30 10:44:15 -0700

    Completed configuration file config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const SECRET_KEY = process.env.SECRET_KEY || "remix-secret-dev";
 
 //by default, backend server runs on port 3001. 
 //NOTE: + converts process.env.port to a number in case it's stored as a string.
-const port = +process.env.port || 3001;
+const PORT = +process.env.port || 3001;
 
 //Use dev database or testing database, according to whether you're running the app locally or running tests.
 function getDatabaseUri() {
@@ -24,5 +24,21 @@ function getDatabaseUri() {
   return (process.env.NODE_ENV === "test") ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL;
 }
 
+//Speed up bcrypt during tests, for dev/production set it to standard work factor of 12 hashing rounds.
+const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
 
+//print out configurations for current runtime instance on terminal.
+//list of environmental variables of interest: SECRET_KEY, PORT, DATABASE_URL, TEST_DATABASE_URL, BCRYPT_WORK_FACTOR
+console.log("RUNNING REMIX. CONFIGURATIONS: ".green);
+console.log("SECRET KEY: ".yellow, SECRET_KEY);
+console.log("Running on port ".yellow, PORT.toString());
+console.log("Bcrypt work factor ".yellow, BCRYPT_WORK_FACTOR);
+console.log("Currently using database with URI address ".yellow, getDatabaseUri());
+console.log("-------------------------------------------------------------------------");
 
+module.exports = {
+  SECRET_KEY,
+  PORT,
+  BCRYPT_WORK_FACTOR,
+  getDatabaseUri
+};
