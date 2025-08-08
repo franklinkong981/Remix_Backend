f4b72e3c4163731148f969d5773ef41a7b9e606d
Franklin Kong <franklinkong981@gmail.com>
2025-08-08 14:19:59 -0700

    Finished part of the dateTime helper function

---
+++ helpers/dateTime.js
@@ -0,0 +1,40 @@
+
+const months = {
+  0: "January",
+  1: "February",
+  2: "March",
+  3: "April",
+  4: "May",
+  5: "June",
+  6: "July",
+  7: "August",
+  8: "September",
+  9: "October",
+  10: "November",
+  11: "December"
+}
+
+function convertToReadableDateTime(sqlTimestamp) {
+  const dateObject = new Date(sqlTimestamp);
+
+  const month = months[dateObject.getMonth()];
+  const day = dateObject.getDate();
+  const year = dateObject.getFullYear();
+  
+  const rawHours = dateObject.getHours();
+  let hours;
+  if (rawHours == 0) {
+    hours = 12;
+  } else if (rawHours <= 12) {
+    hours = rawHours;
+  } else {
+    hours = rawHours - 12;
+  }
+  
+  const minutes = dateObject.getMinutes();
+  const amOrPm = (rawHours < 12) ? "am" : "pm";
+
+  return `${month}/${day}/${year} at ${hours}:${minutes}${amOrPm}`;
+}
+
+console.log(convertToReadableDateTime("2025-08-02 13:07:01.472785"));
