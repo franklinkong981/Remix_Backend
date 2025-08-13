3ff8de11da6dde327bae94eb4d3a97013ffb75c4
Franklin Kong <franklinkong981@gmail.com>
2025-08-13 11:43:00 -0700

    Added Recipe model method that fetches basic info on all recipes

--- models/recipe.js
+++ models/recipe.js
@@ -10,7 +10,18 @@ const { sqlForPartialUpdate } = require("../helpers/sql.js");
 const { NotFoundError, BadRequestError, UnauthorizedError } = require("../errors/errors.js");
 
 class Recipe {
-
+  /** Fetches basic information of all recipes (NOT remixes) in the database and returns them by recipe name in alphabetical order.
+   *  Returns {name, description, imageUrl, createdAt } for each recipe.
+   */
+  static async getAllRecipesBasicInfo() {
+    const allRecipesBasicInfo = await db.query(
+      `SELECT name, description, image_url AS "imageUrl", created_at AS "createdAt"
+       FROM recipes
+       ORDER BY name`
+    );
+    
+    return allRecipesBasicInfo.rows;
+  }
 }
 
 module.exports = Recipe;
