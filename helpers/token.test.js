c4641e40e89b5286fac7a1a52acef331e076738b
Franklin Kong <franklinkong981@gmail.com>
2025-08-27 10:54:38 -0700

    Moved createToken function to token.js

--- helpers/token.test.js
+++ helpers/token.test.js
@@ -1,20 +1,4 @@
-/**  Contains helper functions that aid in token creation. */
+/** Contains tests for the token-related helper functions in token.js that are involved in creating and returning JSON Web tokens. */
 
-const jwt = require("jsonwebtoken");
-const {SECRET_KEY} = require("../config.js");
 
-/** Creates a token by taking the user information inputted (should contain username and email), generates a token 
- *  with the user information input as the payload, signs it with a secret key, and returns is.
- * 
- *  The password must not be supplied, that is private information.
- */
-function createToken({username, email}) {
-  let tokenPayload = {
-    username: username,
-    email: email
-  };
 
-  return jwt.sign(tokenPayload, SECRET_KEY)
-}
-
-module.export = {createToken};
