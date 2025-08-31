a0206c76b33c6b8b98b4cfcd787a32b60aba10ab
Franklin Kong <franklinkong981@gmail.com>
2025-08-31 15:24:19 -0700

    Set up routes/auth.test.js for testing

---
+++ routes/auth.test.js
@@ -0,0 +1,57 @@
+const request = require("supertest");
+
+const app = require("../app.js");
+
+const {
+  commonBeforeAll,
+  commonBeforeEach,
+  commonAfterEach,
+  commonAfterAll,
+} = require("./_testCommon.js");
+
+beforeAll(commonBeforeAll);
+beforeEach(commonBeforeEach);
+afterEach(commonAfterEach);
+afterAll(commonAfterAll);
+
+/************************************** POST /auth/register */
+
+describe("POST /auth/register", function () {
+  test("works for anon", async function () {
+    const resp = await request(app)
+        .post("/auth/register")
+        .send({
+          username: "new",
+          firstName: "first",
+          lastName: "last",
+          password: "password",
+          email: "new@email.com",
+        });
+    expect(resp.statusCode).toEqual(201);
+    expect(resp.body).toEqual({
+      "token": expect.any(String),
+    });
+  });
+
+  test("bad request with missing fields", async function () {
+    const resp = await request(app)
+        .post("/auth/register")
+        .send({
+          username: "new",
+        });
+    expect(resp.statusCode).toEqual(400);
+  });
+
+  test("bad request with invalid data", async function () {
+    const resp = await request(app)
+        .post("/auth/register")
+        .send({
+          username: "new",
+          firstName: "first",
+          lastName: "last",
+          password: "password",
+          email: "not-an-email",
+        });
+    expect(resp.statusCode).toEqual(400);
+  });
+});
