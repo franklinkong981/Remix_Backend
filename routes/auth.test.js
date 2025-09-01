f3dc91b57be1f73a63575eee331cd50085e31f2b
Franklin Kong <franklinkong981@gmail.com>
2025-08-31 17:14:31 -0700

    Added successful tests for POST /auth/register route

--- routes/auth.test.js
+++ routes/auth.test.js
@@ -27,7 +27,6 @@ describe("POST /auth/register", function () {
           email: "user3@gmail.com",
           password: "user3password"
         });
-    console.log(resp.text);
     expect(resp.statusCode).toEqual(201);
     expect(resp.body).toEqual({
       newUserInfo: {
@@ -38,14 +37,39 @@ describe("POST /auth/register", function () {
     });
   });
 
-  test("Throws error if bad request with missing email and password fields", async function () {
+  test("Throws BadRequestError if bad request with missing email and password fields", async function () {
     const resp = await request(app)
         .post("/auth/register")
         .send({
           username: "user3"
         });
-    expect(resp.statusCode).toEqual(400);
+    expect(resp.status).toEqual(400);
+    expect(resp.error.text).toContain('email');
+    expect(resp.error.text).toContain('password');
   });
 
+  test("Throws BadRequestError if bad request with username too short", async function() {
+    const resp = await request(app)
+        .post("/auth/register")
+        .send({
+          username: "u3",
+          email: "user3@gmail.com",
+          password: "user3password"
+        });
+    //console.log(resp.error.text);
+    expect(resp.error.text).toContain("instance.username does not meet minimum length of 5");
+    expect(resp.status).toEqual(400);
+  });
   
+  test("Throws BadRequestError if try to register duplicate username", async function() {
+    const resp = await request(app)
+        .post("/auth/register")
+        .send({
+          username: "user2",
+          email: "user2@gmail.com",
+          password: "user2password"
+        });
+    expect(resp.error.text).toContain("The username user2 is already taken. Please try another username.");
+    expect(resp.status).toEqual(400);
+  });
 });
