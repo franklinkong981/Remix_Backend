923f81bd299643659e0e2a318950c940c184141d
Franklin Kong <franklinkong981@gmail.com>
2025-09-16 11:02:13 -0700

    Wrote successful tests for POST /remixes

--- routes/remixes.test.js
+++ routes/remixes.test.js
@@ -82,4 +82,161 @@ describe("GET /remixes/:remixId works as intended", function() {
   });
 });
 
+/************************************** POST /remixes */
+
+describe("POST /remixes works as intended", function() {
+  test("user1 successfully adds another remix of recipe 1.1 with req.body meeting the correct specifications", async function() {
+    let resp = await request(app).get("/recipes/1/remixes").set("authorization", `${user1Token}`);
+    expect(resp.statusCode).toEqual(200);
+    expect(resp.body.remixes.length).toEqual(1);
+
+    resp = await request(app)
+        .post("/remixes")
+        .send({
+          name: "New remix",
+          description: "Here's some more vegetables to add",
+          ingredients: "Onions, celery, garlic, peppers, corn",
+          directions: "Pour everything into a pan and stir fry it!",
+          purpose: "To add more vegetables",
+          originalRecipeId: 1,
+          cookingTime: 35,
+          servings: 5,
+          imageUrl: "http://pepperscorn.img"
+        })
+        .set("authorization", `${user1Token}`);
+    
+    expect(resp.statusCode).toEqual(201);
+    expect(resp.body.newRemix.name).toEqual("New remix");
+    expect(resp.body.newRemix.description).toContain("Here's some more vegetables to add");
+    expect(resp.body.newRemix.purpose).toContain("more vegetables");
+    expect(resp.body.newRemix.ingredients).toContain("peppers, corn");
+    expect(resp.body.newRemix.directions).toContain("stir fry");
+    expect(resp.body.newRemix.cookingTime).toEqual(35);
+    expect(resp.body.newRemix.servings).toEqual(5);
+    expect(resp.body.newRemix.imageUrl).toEqual(expect.any(String));
+    expect(resp.body.message).toEqual("Successfully added new remix of recipe with id of 1");
+
+    resp = await request(app).get("/recipes/1/remixes").set("authorization", `${user1Token}`);
+    expect(resp.statusCode).toEqual(200);
+    expect(resp.body.remixes.length).toEqual(2);
+    expect(resp.body.remixes[0].name).toEqual("New remix");
+  });
+
+  test("Creates a new remix with just name, description, purpose, originalRecipeId, ingredients, and directions in the request body", async function() {
+    let resp = await request(app)
+        .post("/remixes")
+        .send({
+          name: "New remix",
+          description: "Here's some more vegetables to add",
+          purpose: "To add more vegetables",
+          ingredients: "Onions, celery, garlic, peppers, corn",
+          directions: "Pour everything into a pan and stir fry it!",
+          originalRecipeId: 1
+        })
+        .set("authorization", `${user1Token}`);
+    
+    expect(resp.statusCode).toEqual(201);
+    expect(resp.body.newRemix.cookingTime).toEqual(0);
+    expect(resp.body.newRemix.servings).toEqual(0);
+    expect(resp.body.newRemix.imageUrl).toEqual(expect.any(String));
+
+    resp = await request(app).get("/recipes/1/remixes").set("authorization", `${user1Token}`);
+    expect(resp.statusCode).toEqual(200);
+    expect(resp.body.remixes.length).toEqual(2);
+    expect(resp.body.remixes[0].name).toEqual("New remix");
+  });
+
+  test("Throws BadRequestError if empty body is passed in", async function() {
+    const resp = await request(app)
+        .post("/remixes")
+        .send({})
+        .set("authorization", `${user1Token}`);
+    
+    expect(resp.statusCode).toEqual(400);
+    expect(resp.error.text).toContain("instance requires property");
+    expect(resp.error.text).toContain("name");
+    expect(resp.error.text).toContain("description");
+    expect(resp.error.text).toContain("purpose");
+    expect(resp.error.text).toContain("originalRecipeId");
+    expect(resp.error.text).toContain("ingredients");
+    expect(resp.error.text).toContain("directions");
+  });
+
+  test("Throws BadRequestError if certain strings don't meet proper requirements", async function() {
+    let resp = await request(app)
+        .post("/remixes")
+        .send({
+          name: "New remix",
+          description: "Here's some more vegetables to add",
+          purpose: "",
+          ingredients: "Onions, celery, garlic, peppers, corn",
+          directions: "Pour everything into a pan and stir fry it!",
+          originalRecipeId: 1
+        })
+        .set("authorization", `${user1Token}`);
+    
+    expect(resp.statusCode).toEqual(400);
+    expect(resp.error.text).toContain("instance.purpose does not meet minimum length of 10");
+  });
+
+  test("Throws BadRequestError if cookingTime and/or servings is negative", async function() {
+    let resp = await request(app)
+        .post("/remixes")
+        .send({
+          name: "New remix",
+          description: "Here's some more vegetables to add",
+          purpose: "To add more vegetables",
+          ingredients: "Onions, celery, garlic, peppers, corn",
+          directions: "Pour everything into a pan and stir fry it!",
+          originalRecipeId: 1,
+          cookingTime: -36,
+          servings: 5
+        })
+        .set("authorization", `${user1Token}`);
+    
+    expect(resp.statusCode).toEqual(400);
+    expect(resp.error.text).toContain("instance.cookingTime must be greater than or equal to 0");
+  });
+
+  test("Throws BadRequestError if request body contains attributes outside of the allowed attributes", async function() {
+    let resp = await request(app)
+        .post("/remixes")
+        .send({
+          name: "New remix",
+          description: "Here's some more vegetables to add",
+          ingredients: "Onions, celery, garlic, peppers, corn",
+          directions: "Pour everything into a pan and stir fry it!",
+          purpose: "To add more vegetables",
+          originalRecipeId: 1,
+          rating: 5,
+          cookingTime: 35,
+          servings: 5,
+          imageUrl: "http://pepperscorn.img"
+        })
+        .set("authorization", `${user1Token}`);
+    
+    expect(resp.statusCode).toEqual(400);
+    expect(resp.error.text).toContain("not allowed to have the additional property");
+    expect(resp.error.text).toContain("rating");
+  });
+
+  test("Throws UnauthorizedError if request is sent by user who is not logged in", async function() {
+    let resp = await request(app)
+        .post("/remixes")
+        .send({
+          name: "New remix",
+          description: "Here's some more vegetables to add",
+          ingredients: "Onions, celery, garlic, peppers, corn",
+          directions: "Pour everything into a pan and stir fry it!",
+          purpose: "To add more vegetables",
+          originalRecipeId: 1,
+          rating: 5,
+          cookingTime: 35,
+          servings: 5,
+          imageUrl: "http://pepperscorn.img"
+        });
+    expect(resp.statusCode).toEqual(401);
+    expect(resp.error.text).toContain("You must be logged in to access this!");
+  });
+});
 
