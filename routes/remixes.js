5db508837f51341b3261da2985352f61c0c3621c
Franklin Kong <franklinkong981@gmail.com>
2025-09-16 08:43:45 -0700

    Added route GET /remixes/:remixId

--- routes/remixes.js
+++ routes/remixes.js
@@ -29,5 +29,21 @@ router.get("/:remixId/reviews", ensureLoggedIn, async function(req, res, next) {
   }
 });
 
+/**
+ * GET /remixes/:remixId => { remixDetails: [ {id, remixAuthor, purpose, name, description, originalRecipe, ingredients, directions, cookingTime, servings, reviews: [ {id, reviewAuthor, title, content, createdAt}, ... ], imageUrl, createdAt}, ...] }
+ * 
+ * Endpoint for fetching information for detailed information for a particular recipe, such as the ingredients, instructions, the user who created it, etc. All will be used on the page that displays a recipe's details.
+ * 
+ * Authorization required: Logged in.
+ */
+router.get("/:remixId", ensureLoggedIn, async function(req, res, next) {
+  try {
+    const remixDetails = await Remix.getRemixDetails(req.params.remixId, 3);
+    return res.status(200).json({remixDetails});
+  } catch (err) {
+    return next(err);
+  }
+});
+
 
 module.exports = router;
